ad9554acc954b861ad7466fff255d238fb1c9ecd
Sunspawn <nikolay.babkin@gmail.com>
2021-03-12 17:41:57 +0200

    moved layouts away from the rest

--- app.js
+++ app.js
@@ -5,19 +5,13 @@ const app = express();
 
 //EJS
 app.use(expressLayouts);
+app.set('layout', './layouts/layout')
 app.set('view engine','ejs');
-
 
 //Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
-
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
-
-
-
-
